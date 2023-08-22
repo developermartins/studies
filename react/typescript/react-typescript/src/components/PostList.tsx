a79b48468b19dbc31edb5b54b85dc32ad7bcb196
imartinsCodes <martssilvapro@gmail.com>
2023-08-22 11:40:47 -0300

    fetch data from jsonplaceholder & fix errors

--- react/typescript/react-typescript/src/components/PostList.tsx
+++ react/typescript/react-typescript/src/components/PostList.tsx
@@ -1,22 +1,41 @@
+import { useEffect, useState } from "react";
 import PostCard from "./PostCard"
 
 
 type Props = {}
 
-async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts")
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  };
-
-  return res.json();
-};
-
 const PostList = (props: Props) => {
+
+  const [data, setData] = useState([]);
+
+  
+  useEffect(() => {
+    
+    async function getData() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      };
+    
+      const resJson = await res.json();
+
+      setData(resJson)
+    };
+
+    getData();
+    
+  }, []);
+
+  console.log(data)
+
   return (
     <div>
-      <PostCard title="Lorem Ipsum" body="Dollor sit amet" />
+      {
+        data.map((post: { id: number, title: string, body: string }) => (
+          <PostCard key={ post.id } { ...post }/>
+        ))
+      }
     </div>
   );
 };
