40f8ff9da87eef637be1ebfc6c90b964b20bc422
imartinsCodes <martssilvapro@gmail.com>
2023-08-22 15:12:18 -0300

    TypeScript React events example added

--- react/typescript/react-typescript/src/Pages/Page.tsx
+++ react/typescript/react-typescript/src/Pages/Page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Child from "../components/Children/Child";
 import Parent from "../components/Children/Parent";
 import SecondChild from "../components/Children/SecondChild";
@@ -6,8 +7,27 @@ import SecondChild from "../components/Children/SecondChild";
 type Props = {}
 
 const Page = (props: Props) => {
+
+  // Tipando evento de click
+  const handleClik = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    console.log('Searched!')
+  }
+
+  // Tipando evento de mudanças em inputs
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  }
+
   return (
     <div>
+
+      <form>
+        <input type="text" onChange={ handleChange }/>
+        <button onClick={ handleClik }>Search</button>
+      </form>
+
       <Parent>
         <Child />
         <SecondChild />
