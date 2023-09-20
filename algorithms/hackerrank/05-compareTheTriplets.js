2fbb0c446acc895883c21fe0f0c0768f8996af93
imartinsCodes <martssilvapro@gmail.com>
2023-09-20 15:00:07 -0300

    compare the triplets fished

--- algorithms/hackerrank/05-compareTheTriplets.js
+++ algorithms/hackerrank/05-compareTheTriplets.js
@@ -1,4 +1,17 @@
 function compareTriplets(a, b) {
+  let aliceScore = 0;
+  let bobScore = 0;
 
+  for (let index = 0; index < a.length; index++) {
+    if(a[index] > b[index]) {
+      aliceScore++
+    } else if (a[index] < b[index]) {
+      bobScore++
+    };
+    
+  };
+
+  return [aliceScore, bobScore];
 };
 
+console.log(compareTriplets([17, 28, 30], [99, 16, 8]));
