0e7b99fbaf70ee9854fadc8f20666ac9795b5c6c
imartinsCodes <martssilvapro@gmail.com>
2023-09-20 12:20:10 -0300

    secound example added

--- algorithms/hackerrank/04-simpleArraySum.js
+++ algorithms/hackerrank/04-simpleArraySum.js
@@ -1,4 +1,22 @@
 function simpleArraySum(ar) {
 
   return ar.reduce((acumulador, valorInicial) => acumulador + valorInicial, 0);
-}
+};
+
+//==========================
+
+// OR
+
+function simpleArraySum(ar) {
+  let sum = 0;
+
+  for (let index = 0; index < ar.length; index++) {
+    
+    sum = sum + ar[index];
+
+  };
+
+  return sum;
+};
+
+console.log(simpleArraySum([1, 2, 3]));
