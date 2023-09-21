44eb80490f59b5bf05f4d1009664a7d74a9fb07e
imartinsCodes <martssilvapro@gmail.com>
2023-09-21 10:12:21 -0300

    aVeryBigSum secound solution added

--- algorithms/hackerrank/06-aVeryBigSum.js
+++ algorithms/hackerrank/06-aVeryBigSum.js
@@ -2,3 +2,25 @@ function aVeryBigSum(ar) {
   return ar.reduce((iterator, actualValue) => iterator + actualValue)
 };
 
+let arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+console.log(aVeryBigSum(arr));
+
+// ===================================
+
+// OR
+
+function aVeryBigSum(ar) {
+  let bigSum = 0;
+
+  for (let index = 0; index < ar.length; index++) {
+    
+    bigSum = bigSum + ar[index]
+  }
+
+  return bigSum
+}
+
+let arr2 = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+console.log(aVeryBigSum(arr2))
