efa2fe513334cd341b3f8345beced70b9d06f69f
developermartins <martssilvapro@gmail.com>
2025-02-19 14:00:41 -0300

    file added

---
+++ science/oop/characterEncapsulado.ts
@@ -0,0 +1,29 @@
+class CharacterEncapsulado {
+  name: string;
+  height: number;
+  race: string;
+
+  constructor (n: string, h: number, r: string) {
+    this.name = n;
+    this.height = h;
+    this.race = r;
+  }
+
+  walk() {
+    console.log("🚶‍♀️‍➡️🚶‍♀️‍➡️🚶‍♀️‍➡️🚶‍♀️‍➡️🚶‍♀️‍➡️🚶‍♀️‍➡️🚶‍♀️‍➡️");
+  }
+
+  attack() {
+    console.log("🤺")
+  }
+}
+
+// const hero1 = new Character('Martius', 1.90, 'human');
+
+// const hero2 = new Character('??', 1.90, 'elf');
+
+// console.log(hero1.name, hero1.race, hero1.height);
+
+// console.log(hero2.name, hero2.race);
+
+// console.log(hero1.attack, `${hero1.name}  atacou o ${hero2.name}`);
